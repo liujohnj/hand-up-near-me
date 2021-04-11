6b1c9673cf39dcc4e90a52338bac401f76f123ec
larrylim1996 <76197142+larrylim1996@users.noreply.github.com>
2021-04-10 19:19:33 -0500

    Delete backend directory

--- backend/util/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    databaseKey: "test-ygbzv",
-    databaseURI: "mongodb+srv://Larry:<0123456789>@handupnearme.bueq4.mongodb.net/HandUpNearMe?retryWrites=true&w=majority",
-    projectID: "HandsUpNearMe"
-}
--- backend/util/db.js
+++
@@ -1,7 +0,0 @@
-/*
-exports.db = test.database.db("handsUpNearMe");
-exports.user = database.db("handsUpNearMe").collection("user");
-exports.provider = database.db("handsUpNearMe").collection("provider");
-exports.handsUp = database.db("handsUpNearMe").collection("handsUp");
-
-*/
