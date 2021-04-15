7f2c83e268d6abc51d2477960026893356bd01ec
liujohnj <48953359+liujohnj@users.noreply.github.com>
2021-04-15 02:02:48 -0400

    feat: update one field (zipCode) on MongoDb back-end

--- front-end/src/EditProfile.js
+++ front-end/src/EditProfile.js
@@ -1,85 +1,84 @@
-
-
 import { useParams } from "react-router-dom";
 import CalcDistance from "./CalcDistance";
 import MapLocation from "./MapLocation";
 import ReturnServices from "./ReturnServices";
 import useFetch from "./useFetch";
 import { useState, useEffect, useLocalStorage } from 'react';
+//import axios from 'axios';
+
+
 
 
 const EditProfile = () => {
     const { id } = useParams();
     const { providers, error, isPending } = useFetch('http://localhost:5000/providers/' + id)
 
-    
-    /*
-    const defaultStrCheckboxes = JSON.stringify(defaultCheckboxes);
 
-    const checkboxesFromLocalStorage = JSON.parse(localStorage.getItem('myCheckboxes') || defaultStrCheckboxes);
-    const [state, setState] = useState(checkboxesFromLocalStorage);
-
-    useEffect(() => {
-        localStorage.setItem('myCheckboxes', JSON.stringify(state))
-    }, [state]);
-
-    function handleChange(e) {
-        const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
-        setState({
-            ...state,
-            [e.target.name]: value
-        });
+    const [zipcode, setzipCode] = useState("33701");
+  
+    const mypost = {
+        zipCode: zipcode
     }
-    */
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(mypost),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const handleChangeZipCode = e => {
+        setzipCode(e.target.value);
+        console.log("one: ", zipcode);
+       
+    }
+
+    const handleSubmit = e => {
+        fetch('http://localhost:5000/providers/update/607786327c5e2f902d2d55f0', options)
+            .then(res => res.json())
+            .then(res => console.log("res: ", res));
+        console.log("two: ", zipcode);
+    }
+
+
     return (
         <div className="provider-details">
             { isPending && <div>Loading...</div>}
             { error && <div>{ error }</div>}
             { providers && (
                 <article>
-                    <form>
-                        <label>
-                            Provider Name:
-                            <br/>
-                            <input type="text" size="50" name="name" placeholder={ providers.name } />
-                        </label>
-                        <input type="submit" value="Submit" />
-                    </form>
-                    
-                    <br/>
-                    <br/>
-
-                    <p>{ providers.address1 }</p>
-                    <p>{ providers.address2 }</p>
-                    <p>{ providers.city + ", " + providers.state + " " + providers.zipCode }</p>
-                    <p>{ "Tel: " + providers.phone }</p>
-                    <br />
-                    <form>
-                        <label>
-                            Text1:
-                            <br/>
-                            <textarea rows="5" cols="80" name="text1" placeholder={ providers.text1 } />
-                            <br/>
-                            <input type="submit" value="Submit" />
-                        </label>
-                    </form>
-
-                    <form>
-                    <label>
-                        <input
-                            type="checkbox"
-                            name="hookChildcare"
-                            checked={providers.hasAdoption}
-                            //onChange={handleChange}
-                        />
-                        Adoption
-                    </label>
+                    <form onSubmit={handleSubmit}>
+                        Provider Name:
+                        <input type="text" size="50" name="name" placeholder={ providers.name } />
+                        <br />
+                        <br />
+                        Street address:
+                        <input type="text" size="50" name="address1" placeholder={ providers.address1 } />
+                        <br />
+                        <br />
+                        Suite #:
+                        <input type="text" size="50" name="address2" placeholder={ providers.address2 } />
+                        <br />
+                        <br />
+                        City:
+                        <input type="text" size="50" name="city" placeholder={ providers.city } />
+                        <br />
+                        <br />
+                        State:
+                        <input type="text" size="50" name="state" placeholder={ providers.state } />
+                        <br />
+                        <br />
+                        Zip code:
+                        <input type="text" size="50" name="zip" placeholder={ providers.zipCode } onChange={handleChangeZipCode} />
+                        <input type="submit" value="update" />
+                        <br />
+                        <br />
                     </form>
                 </article>
             )}
             <br />
             <ReturnServices provider={ providers } />
-            <MapLocation provider={ providers } />
         </div>
     );
 }
