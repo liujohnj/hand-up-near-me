a4c4f669a53de5e9599f44081f9c01ce8c424112
liujohnj <48953359+liujohnj@users.noreply.github.com>
2021-04-14 23:00:37 -0400

    refactor: rename id field to pid

--- back-end/models/provider.model.js
+++ back-end/models/provider.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const providerSchema = new Schema({
-  id: { type: Number },
+  pid: { type: Number },
   username: { type: String },
   passwordHash: { type: String },
   name: { type: String },
