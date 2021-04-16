f1a6e8aabac0e33f752171342b7ce52b8ce06e03
liujohnj <48953359+liujohnj@users.noreply.github.com>
2021-04-15 21:53:39 -0400

    feat: continue working on fix for updating back-end

--- front-end/src/EditProfile.js
+++ front-end/src/EditProfile.js
@@ -1,77 +1,78 @@
 import { useParams } from "react-router-dom";
-import CalcDistance from "./CalcDistance";
-import MapLocation from "./MapLocation";
 import ReturnServices from "./ReturnServices";
-import useFetch from "./useFetch";
-import { useState, useEffect, useLocalStorage } from 'react';
-//import axios from 'axios';
-
-
-
+import { useState, useEffect } from 'react';
 
 const EditProfile = () => {
+   
+    const [providers, setProviders] = useState([]);
     const { id } = useParams();
-    const { providers, error, isPending } = useFetch('http://localhost:5000/providers/' + id)
+    const url = ('http://localhost:5000/providers/' + id);
 
-
-    const [zipcode, setzipCode] = useState("33701");
-  
-    const mypost = {
-        zipCode: zipcode
-    }
+    useEffect(() => {
+       const fetchData = async () => {
+           const response = await fetch(url);
+           const newData = await response.json();
+           setProviders(newData);
+        };
+        fetchData();
+    }, []);
 
     const options = {
         method: 'POST',
-        body: JSON.stringify(mypost),
+        body: JSON.stringify(
+            {
+                "name":providers.name,
+                "address1":providers.address1,
+                "address2":providers.address2,
+                "city":providers.city,
+                "state":providers.state,
+                "zipCode":providers.zipCode
+            }),
         headers: {
             'Content-Type': 'application/json'
         }
     };
 
-    const handleChangeZipCode = e => {
-        setzipCode(e.target.value);
-        console.log("one: ", zipcode);
-       
+    const handleChangeTextField = e => {
+        setProviders({ [e.target.name] : e.target.value });
     }
 
-    const handleSubmit = e => {
-        fetch('http://localhost:5000/providers/update/607786327c5e2f902d2d55f0', options)
+    const handleSubmitTextFields = e => {
+        fetch('http://localhost:5000/providers/update/' + id, options)
             .then(res => res.json())
             .then(res => console.log("res: ", res));
-        console.log("two: ", zipcode);
     }
-
-
+    
     return (
         <div className="provider-details">
-            { isPending && <div>Loading...</div>}
-            { error && <div>{ error }</div>}
             { providers && (
                 <article>
-                    <form onSubmit={handleSubmit}>
+                    <form>
                         Provider Name:
-                        <input type="text" size="50" name="name" placeholder={ providers.name } />
+                        <input type="text" size="50" name="name" placeholder={ providers.name } onChange={handleChangeTextField} />
                         <br />
                         <br />
                         Street address:
-                        <input type="text" size="50" name="address1" placeholder={ providers.address1 } />
+                        <input type="text" size="50" name="address1" placeholder={ providers.address1 } onChange={handleChangeTextField} />
                         <br />
                         <br />
                         Suite #:
-                        <input type="text" size="50" name="address2" placeholder={ providers.address2 } />
+                        <input type="text" size="50" name="address2" placeholder={ providers.address2 } onChange={handleChangeTextField} />
                         <br />
                         <br />
                         City:
-                        <input type="text" size="50" name="city" placeholder={ providers.city } />
+                        <input type="text" size="50" name="icity" placeholder={ providers.city } onChange={handleChangeTextField}/>
                         <br />
                         <br />
                         State:
-                        <input type="text" size="50" name="state" placeholder={ providers.state } />
+                        <input type="text" size="50" name="istate" placeholder={ providers.state } onChange={handleChangeTextField}/>
                         <br />
                         <br />
                         Zip code:
-                        <input type="text" size="50" name="zip" placeholder={ providers.zipCode } onChange={handleChangeZipCode} />
-                        <input type="submit" value="update" />
+                        <input type="text" size="50" name="zipCode" placeholder={ providers.zipCode } onChange={handleChangeTextField} />
+                        <br />
+                        <br />
+                        <input type="submit" value="update" onClick={handleSubmitTextFields} />
                         <br />
                         <br />
                     </form>
