87d4084c395ac97970a2fb53cc8f8ee0ef3a171a
Kaitlyn Robey <robeyk@ufl.edu>
2021-04-16 16:22:39 -0400

    Moved files

--- server.js
+++ server.js
@@ -1,49 +1,27 @@
-// Specification for http server
-var http = require('http'), 
-	fs = require('fs'), 
- 	url = require('url'),
-  	port = 8080;
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-/* Global variables */
-var listingData, server;
+require('dotenv').config();
 
-var requestHandler = function(request, response) {
-  	var parsedUrl = url.parse(request.url);
+const app = express();
+const port = process.env.PORT || 5000;
 
-  	/*
-    	This request handler should send listingData in the JSON format if a GET request 
-    	is sent to the '/listings' path. Otherwise, it should send a 404 error.
-    
-   */
-   
-   if (parsedUrl.pathname == '/listings') {
-  		response.write(listingData);
-  		response.end();
-  	} 
-  	
-  	else {
-  		response.writeHead(404, {"Content-Type": "text/plain"});
-  		response.write('404, Page Not Found');
-  		response.end();
-  	}
-};
+app.use(cors());
+app.use(express.json());
 
-// Server initialization
-var server = http.createServer(requestHandler);
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
 
-fs.readFile('listings.json', 'utf8', function(err, data) {
-  	/*
-   	This callback function should save the data in the listingData variable, 
-    	then start the server. 
-   */
-   // error and data handling
-  	if(err) throw err;
-  
-  	listingData = data;
+const providersRouter = require('./back-end/routes/providers');
 
-	// Start the server
-  	server.listen(port, function() {
-		console.log('Server listening on: http://127.0.0.1:' + port);   
-	});
-   
+app.use('/providers', providersRouter);
+
+app.listen(port, () => {
+    console.log('Server is running on port: ${port}');
 });
