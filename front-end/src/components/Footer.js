34d28b4d3eee67a71e56fa0296c32dffaf92b336
Kaitlyn Robey <robeyk@ufl.edu>
2021-04-21 18:24:33 -0400

    Added footer and home page title

---
+++ front-end/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <div style = {{
+            backgroundColor: 'lightgray',
+            marginTop: '20px',
+            textAlign: 'center'
+        }}>
+            <p>Hand Up Near Me 2021</p>
+            <p>Home</p>
+            <p>About</p>
+
+        </div>
+    );
+}
+
+export default Footer;
--- front-end/src/components/HomeTextTop.js
+++ front-end/src/components/HomeTextTop.js
@@ -3,24 +3,17 @@ import { Link } from 'react-router-dom';
 
 const HomeTextTop = () => {
     return (
-        <div>
-            <h2>Are you looking for services?</h2>
-            
-            <p>If you are in need of free or reduced-fee services, it can be difficult to find providers in your area that meet your criteria.</p>
-
-            <p>At Hand Up Near Me, our goal is to gather services in one convenient place so you can find them easily. You can search for providers by name, by category, or by distance.</p>
-
-            <h4>Click below to begin your search for free.</h4>
-
-            <Link to="/search" style ={{
-                color: "white",
-                backgroundColor: '#f1356d',
-                borderRadius: '8px',
-                padding: '5px'
-            }}>Search for Services Near You</Link>
-           
+        <div style = {{
+            textAlign: 'center'
+        }}>
+            <h1>Hand Up Near Me</h1>
+            <p style = {{
+                color: '#f1356d',
+                fontSize: '20px'
+            }}>Finding help can be hard. We're here to lend a hand.</p>
         </div>
+        
     );
 }
 
-export default HomeTextLeft;
+export default HomeTextTop;
