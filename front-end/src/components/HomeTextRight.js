d8a6673e025585de013acc23e1b95ddd424a2115
liujohnj <48953359+liujohnj@users.noreply.github.com>
2021-04-21 22:52:57 -0400

    fix: wrong link

--- front-end/src/components/HomeTextRight.js
+++ front-end/src/components/HomeTextRight.js
@@ -19,7 +19,7 @@ const HomeTextRight = () => {
 
             <br />
             <center>
-                <Button variant="outline-primary" href="/search">Create a Provider Account</Button>{' '}
+                <Button variant="outline-primary" href="/dashboard">Create a Provider Account</Button>{' '}
             </center>
             <br />
 
